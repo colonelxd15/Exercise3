44ca32fd8a276469aa70c97422ce1c04a245ef7a
Norman Manantan <normanmanantan@gmail.com>
2014-04-28 15:20:48 +0800

    Slider ok

--- js/jquery.slider.js
+++ js/jquery.slider.js
@@ -1,43 +1,46 @@
 jQuery(document).ready(function ($) {
 	
-	var time = setInterval(function(){
-			$('#holder').fadeOut(500,move).fadeIn(500);
-		},3000);
-	
 	var position;
 	var count;
 	var currentpos = 0;
-	
-	$('#slidecounter img').on('click', function(){
-		position = $(this).index();
-		while(position > 0)
-		{
-			$('#holder img:last').prependTo('#holder');
-			$('#slidecounter img:first').appendTo('#slidecounter');
-			position--;
-		}
-	});
+	var x = 0;
+	var time = setInterval(function(){
+			$('#holder').fadeOut(500,move2).fadeIn(500);
+		},3000);
 
-	$('#holder img').hover(function(){
+	$('#holder img, #slidecounter').hover(function(){
 		clearInterval(time);
 		time ='';
 		},function(){
 			fade();
 		}
 	)
+	
+	$('#slidecounter span').on('click', function(){
+		position = $(this).index();
+		$('#holder').fadeOut(500,function(){
+			$('#holder').css('margin-left',position * -400 + 'px');
+			$('#slidecounter span').attr('class','black');
+			$('#slidecounter span').removeAttr('id','white');
+			$($('#slidecounter span')[position]).attr('id','white');
+		}).fadeIn(500);
+	});
 
-	function move(){
-		$('#holder img:first').appendTo('#holder');
-		$('#slidecounter img:last-child').prependTo('#slidecounter');
-	};
+	function move2(){
+		position = currentpos + 1;
+		$('#holder').css('margin-left',position * -400 + 'px');
+		
+		currentpos++;
+		if(currentpos > 2)
+		{
+			currentpos = -1;
+		}
+		$('#slidecounter span:last-child').prependTo('#slidecounter');
+	}
 
 	function fade(){
 		time = setInterval(function(){
-			$('#holder').fadeOut(500,move).fadeIn(500);
-			currentpos++;
-			if (currentpos>3) {
-				currentpos = 0;
-			};
+			$('#holder').fadeOut(500,move2).fadeIn(500);
 		},3000);
 	}
 
