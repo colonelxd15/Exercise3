a2568d59037561d5ef946da58ebe6af7d102dd9a
Norman Manantan <normanmanantan@gmail.com>
2014-04-28 14:35:09 +0800

    color changed

--- js/jquery.slider.js
+++ js/jquery.slider.js
@@ -1,18 +1,44 @@
 jQuery(document).ready(function ($) {
 	
-	var time;
+	var time = setInterval(function(){
+			$('#holder').fadeOut(500,move).fadeIn(500);
+		},3000);
+	
+	var position;
+	var count;
+	var currentpos = 0;
+	
+	$('#slidecounter img').on('click', function(){
+		position = $(this).index();
+		while(position > 0)
+		{
+			$('#holder img:last').prependTo('#holder');
+			$('#slidecounter img:first').appendTo('#slidecounter');
+			position--;
+		}
+	});
 
-	$('#imageslider').load(fade());
+	$('#holder img').hover(function(){
+		clearInterval(time);
+		time ='';
+		},function(){
+			fade();
+		}
+	)
+
+	function move(){
+		$('#holder img:first').appendTo('#holder');
+		$('#slidecounter img:last-child').prependTo('#slidecounter');
+	};
 
 	function fade(){
 		time = setInterval(function(){
-			$('#holder').fadeOut("slow",
-				function(){
-					$('#holder img:first').appendTo('#holder');
-				}).fadeIn("slow");
-			$('#slidecounter img:last-child').prependTo('#slidecounter');
-	
-		},2000);
-	};
+			$('#holder').fadeOut(500,move).fadeIn(500);
+			currentpos++;
+			if (currentpos>3) {
+				currentpos = 0;
+			};
+		},3000);
+	}
 
 });
